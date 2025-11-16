1a0d178cfbe0f304e85a799db548630f4c0f3863
SHRUJAN S <shrujans17@gmail.com>
2025-11-16 12:02:21 +0530

    Jump Statement
    we are using break and continue jump statement in for loop.

---
+++ jumpstatement.java
@@ -0,0 +1,19 @@
+public class jumpstatement {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            
+            if (i == 5) {
+                continue;
+            }
+
+          
+            if (i == 9) {
+                System.out.println("Reached 9, breaking the loop.");
+                break;
+            }
+
+            
+            System.out.println("Number: " + i);
+        }
+    }
+}
